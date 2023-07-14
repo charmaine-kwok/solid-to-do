d1f44ca8e6c28a4ac094b81e4e83fc6b2464bc41
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-14 18:33:09 +0800

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js", // configure the Flowbite JS source template paths
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
