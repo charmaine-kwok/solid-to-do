ccf7fca90beb9d74f5db92ba5803b47458a5ba5e
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-14 16:57:36 +0800

    seperate fetchData function

---
+++ src/functions/fetchData.tsx
@@ -0,0 +1,16 @@
+export default async function fetchData() {
+  try {
+    const [todoResponse, doneResponse] = await Promise.all([
+      fetch("http://localhost:8080/todo/todos"),
+      fetch("http://localhost:8080/done/dones"),
+    ]);
+
+    const todoData = await todoResponse.json();
+    const doneData = await doneResponse.json();
+
+    return { todoData, doneData };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { todoData: null, doneData: null };
+  }
+}
