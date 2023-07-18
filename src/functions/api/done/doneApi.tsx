510e32487b9a8d842e51cb16d013854c0140911d
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-18 12:08:19 +0800

    add addDone function

--- src/functions/api/done/doneApi.tsx
+++ src/functions/api/done/doneApi.tsx
@@ -1,9 +1,26 @@
+import { removeTodo } from "../todo/todoApi";
+
+export async function addDone(id: number, todo: string) {
+  await removeTodo(id);
+  try {
+    await fetch("http://localhost:8080/done", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ subject: todo }),
+    });
+  } catch (error) {
+    console.error("Error adding done:", error);
+  }
+}
+
 export async function removeDone(id: number) {
   try {
     await fetch(`http://localhost:8080/done/${id}`, {
       method: "DELETE",
     });
   } catch (error) {
-    console.error("Error removind done:", error);
+    console.error("Error removing done:", error);
   }
 }
